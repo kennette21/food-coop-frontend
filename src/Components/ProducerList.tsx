387c34812b15592730f96c87ab01dce910a3d0eb
Dallin Elmer <djelmer@iastate.edu>
2020-01-17 20:19:40 -0600

    Adding react-bootstrap and making Product List a table. If react-bootstrap ends up sucking then we will remove it in the future

--- src/Components/ProducerList.tsx
+++ src/Components/ProducerList.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import ProducerListItem, { ProducerListItemProps } from './ProducerListItem';
 import styled from 'styled-components';
+import Table from 'react-bootstrap/Table'
 
-const List = styled.ul`
-    border: 2px solid black;
-    width: 600px;
-    margin: auto;
-`;
+// const List = styled.ul`
+//     border: 2px solid black;
+//     width: 600px;
+//     margin: auto;
+// `;
 
-const ListContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    margin: auto;
-`;
+ const ListContainer = styled.div`
+     display: flex;
+     flex-direction: column;
+     margin: auto;
+ `;
 
 class ProducerList extends React.Component {
     render() {
@@ -34,9 +35,19 @@ class ProducerList extends React.Component {
         return (
             <ListContainer>
                 <h2>Producer List</h2>
-                <List>
-                    {this.renderListItems(mockedListData)}
-                </List>
+                <Table striped bordered hover variant="dark">
+                    <thead>
+                        <tr>
+                            <th>Logo</th>
+                            <th>Name</th>
+                            <th>Description</th>
+                            <th>Number of Products</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.renderListItems(mockedListData)}
+                    </tbody>
+                </Table>
             </ListContainer>
         )
     }
--- src/Components/ProducerListItem.tsx
+++ src/Components/ProducerListItem.tsx
@@ -1,23 +1,22 @@
 import React from 'react';
-import styled from 'styled-components';
+//import styled from 'styled-components';
+//import ProducerList from './ProducerList';
 
-const ItemContainer = styled.li`
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    border-bottom: 2px solid gray;
-`;
+//  const ItemContainer = styled.td`
+//      justify-content: space-between;
+//      border-bottom: 2px solid gray;
+//  `;
 
-const BaseItemContent = styled.div`
-    border-right: 1px solid gray;
-    display: flex;
-    align-items: center;
-    width: 100px;
-`;
+// const BaseItemContent = styled.div`
+//     border-right: 1px solid gray;
+//     display: flex;
+//     align-items: center;
+//     width: 100px;
+// `;
 
-const Name = styled(BaseItemContent)`
-    font-size: 16px;
-`;
+// const Name = styled(BaseItemContent)`
+//     font-size: 16px;
+// `;
 
 export type ProducerListItemProps = {
     imageSrc: string;
@@ -28,18 +27,33 @@ export type ProducerListItemProps = {
 
 class ProducerListItem extends React.Component<ProducerListItemProps, {}> {
     render() {
-        const {imageSrc, name, description, numProducts} = this.props;
+        const { imageSrc, name, description, numProducts } = this.props;
 
         return (
-            <ItemContainer className="product-list-item-container">
-                <img className="item-image" src={imageSrc} alt="text"/>
-                <Name className="item-name">{name}</Name>
-                <BaseItemContent className="item-description">{description}</BaseItemContent>
-                <BaseItemContent className="num-products">{numProducts}</BaseItemContent>
-                <button className="item-action-button"></button>
-            </ItemContainer>
-        )
+            <tr>
+                <td><img className="item-image" src={imageSrc} alt="text" /></td>
+                <td>{name}</td>
+                <td>{description}</td>
+                <td>{numProducts}</td>
+            </tr>
+        );
     }
 }
 
 export default ProducerListItem;
+
+// class ProducerListItem extends React.Component<ProducerListItemProps, {}> {
+//     render() {
+//         const {imageSrc, name, description, numProducts} = this.props;
+
+//         return (
+//             <ItemContainer className="product-list-item-container">
+//                 <img className="item-image" src={imageSrc} alt="text"/>
+//                 <Name className="item-name">{name}</Name>
+//                 <BaseItemContent className="item-description">{description}</BaseItemContent>
+//                 <BaseItemContent className="num-products">{numProducts}</BaseItemContent>
+//                 <button className="item-action-button"></button>
+//             </ItemContainer>
+//         )
+//     }
+// }
